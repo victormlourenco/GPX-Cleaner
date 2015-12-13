cffb4a76e092cd0a95174e29afd2bd0b67b02c8a
victormlourenco <victormlourenco@gmail.com>
2015-12-13 15:37:54 -0200

    Removido atributo não utilizado

--- src/gpx/Ponto.java
+++ src/gpx/Ponto.java
@@ -7,15 +7,6 @@ public class Ponto {
 	private double longitude;
 	private Object frame;
 	private double distancia;
-	private double distanciaProx;
-
-	public double getDistanciaProx() {
-		return distanciaProx;
-	}
-
-	public void setDistanciaProx(double distanciaProx) {
-		this.distanciaProx = distanciaProx;
-	}
 
 	// Constrói um objeto do tipo Ponto através de um objeto do tipo frame
 	public Ponto(Object frame) {
@@ -32,7 +23,6 @@ public class Ponto {
 		this.longitude = 0;
 		this.frame = null;
 		this.distancia = Double.MAX_VALUE;
-		this.distanciaProx = Double.MAX_VALUE;
 	}
 
 	// Constrói um a partir de um y,x
@@ -42,7 +32,6 @@ public class Ponto {
 		this.longitude = longitude;
 		this.frame = null;
 		this.distancia = Double.MAX_VALUE;
-		this.distanciaProx = Double.MAX_VALUE;
 	}
 
 	public double getLatitude() {
